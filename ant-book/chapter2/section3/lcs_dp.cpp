3ae20c2bd5d6739b0e4c032fb3dfffce2f5ec7c6
elasticnet12345 <elasticnet1995@gmail.com>
2017-10-26 15:27:04 +0900

    lcs_dp

--- ant-book/chapter2/section3/lcs_dp.cpp
+++ ant-book/chapter2/section3/lcs_dp.cpp
@@ -19,3 +19,11 @@ void solve(){
   for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
       if(s[i] == t[j]){
+	dp[i+1][j+1] = dp[i][j]+1;
+      }else{
+	dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+      }
+    }
+  }
+  cout << dp[n][m] << endl;
+}
